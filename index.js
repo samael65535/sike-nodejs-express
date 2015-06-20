d0b2d573ff79f6517ec931585c0550201f836973
samael <samael.65535@gmail.com>
2015-06-20 13:08:28 +0800

    refactor

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var http = require('http');
-module.exports = function() {
-    var server = http.createServer(function(req, res) {
-        res.writeHead(404);
-        res.end("hello")
-    });
+
+var proto =  function(req, res) {
+    res.writeHead(404);
+    res.end("hello")
+};
+
+proto.listen = function() {
+    var server = http.createServer(this);
+    server.listen.apply(server, arguments)
     return server
+};
+
+module.exports = function() {
+    return proto;
 };

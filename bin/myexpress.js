843918a44c997f9a202315414c1ee06dd4629556
samael <samael.65535@gmail.com>
2015-06-24 22:28:34 +0800

    add path-to-regexp

--- bin/myexpress.js
+++ bin/myexpress.js
@@ -1,16 +1,6 @@
 #!/usr/bin/env node
 var express = require("../index");
 var app = express();
-app.use("/foo",function(req,res,next) {
-    throw "boom!"
-});
 
-app.use("/foo/a",function(err,req,res,next) {
-    res.end("error handled /foo/a");
-});
-
-app.use("/foo/b",function(err,req,res,next) {
-    res.end("error handled /foo/b");
-});
 app.listen(4000);
 

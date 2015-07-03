2be1a32b85dbc8ccf6ee4f70b40fd068a2db63e5
samael <samael.65535@gmail.com>
2015-07-03 23:04:21 +0800

    Implement Injector Invokation

--- bin/myexpress.js
+++ bin/myexpress.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 var express = require("../index");
 var makeRoute = require('../lib/route');
+var inject = require("../lib/injector");
 var app = express();
-var route = app.route("/foo")
-    .get(function(req,res,next) {
-        next();
-    })
-    .get(function(req,res) {
-        res.end("foo");
-    });
-app.listen(4000);
+var req = 1,
+    res = 2;
+function next(err) {
+    expect(err).to.be.instanceof(Error);
+    expect(err.message).to.equal("Factory not defined: unknown_dep");
+}
+
+function handler(unknown_dep) {};
+injector = inject(handler,app);
+injector(req,res,next);
+//app.listen(4000);

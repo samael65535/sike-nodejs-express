45667a8c4c457062b58ec4a005fcf57dea6d9fb3
samael <samael.65535@gmail.com>
2015-07-06 23:36:41 +0800

    HTTP redirect

--- bin/myexpress.js
+++ bin/myexpress.js
@@ -3,32 +3,11 @@ var express = require("../index");
 var makeRoute = require('../lib/route');
 var inject = require("../lib/injector");
 var app = express();
-
-//var req = 1,
-//    res = 2,
-//    next = 3;
-//function handler(res,foo) {
-//    console.log(res, foo);
-//}
-
-//injector = inject(handler,app);
-//injector(req,res,next);
-var subapp = express();
-var _app, _subapp;
-subapp.use(function(req,res,next) {
-    _subapp = req.app; // => subapp
-    console.log(subapp === _subapp);
-    next();
+app = express();
+app.use("/foo",function(req,res) {
+    res.redirect("/baz"); // default is 302
 });
-app.use(subapp);
-app.use(function(req,res) {
-    _app = req.app;
-    console.log(app === _app);
-    console.log(_app === subapp);
-    res.end("ok");
+app.use("/bar",function(req,res) {
+    res.redirect(301,"/baz");
 });
-
-//app.use(function(req,res) {
-//    res.end(req.isExpress + "," + res.isExpress);
-//});
 app.listen(4000);

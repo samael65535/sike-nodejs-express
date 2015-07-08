ea8c84d02e146f55c92fc0b524b3e76992fd69d8
samael <samael.65535@gmail.com>
2015-07-09 00:36:19 +0800

    Respond with different formats

--- bin/myexpress.js
+++ bin/myexpress.js
@@ -4,10 +4,15 @@ var makeRoute = require('../lib/route');
 var inject = require("../lib/injector");
 var app = express();
 app = express();
-app.use("/foo",function(req,res) {
-    res.redirect("/baz"); // default is 302
-});
-app.use("/bar",function(req,res) {
-    res.redirect(301,"/baz");
+app.use(function(req,res) {
+    res.format({
+        text: function() {
+            res.end("text hello");
+        },
+
+        html: function() {
+            res.end("html <b>hello</b>");
+        }
+    });
 });
 app.listen(4000);

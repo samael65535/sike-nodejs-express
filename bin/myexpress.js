ecf14fcb6be1f1008100a9a8d5abf0d04cd77763
samael <samael.65535@gmail.com>
2015-07-14 23:42:52 +0800

    delete debug code

--- bin/myexpress.js
+++ bin/myexpress.js
@@ -3,9 +3,4 @@ var express = require("../index");
 var makeRoute = require('../lib/route');
 var inject = require("../lib/injector");
 var app = express();
-app = express();
-app.use(function(req,res) {
-    res.format({
-    });
-});
 app.listen(4000);

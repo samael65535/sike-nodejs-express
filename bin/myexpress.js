ab64e67051702a9fa94b339b9a8d07e1964f34e9
samael <samael.65535@gmail.com>
2015-07-15 23:25:40 +0800

    support buffer and string body

--- bin/myexpress.js
+++ bin/myexpress.js
@@ -3,4 +3,17 @@ var express = require("../index");
 var makeRoute = require('../lib/route');
 var inject = require("../lib/injector");
 var app = express();
+app.use("/buffer",function(req,res) {
+    res.send(new Buffer("binary data"));
+    // Content-Type: application/octet-stream
+});
+app.use("/string",function(req,res) {
+    res.send("string data");
+    // Content-Type: text/html
+});
+app.use("/json",function(req,res) {
+    res.type("json");
+    res.send("[1,2,3]");
+    // Content-Type: application/json
+});
 app.listen(4000);

bbf3ce8808280b0f80d9035b814ee3422f385c4b
samael <samael.65535@gmail.com>
2015-07-19 01:14:46 +0800

    refactor

--- bin/myexpress.js
+++ bin/myexpress.js
@@ -3,17 +3,11 @@ var express = require("../index");
 var makeRoute = require('../lib/route');
 var inject = require("../lib/injector");
 var app = express();
-app.use("/buffer",function(req,res) {
-    res.send(new Buffer("binary data"));
-    // Content-Type: application/octet-stream
+app.use("/plumless",function(req,res) {
+    res.send("plumless");
 });
-app.use("/string",function(req,res) {
-    res.send("string data");
-    // Content-Type: text/html
-});
-app.use("/json",function(req,res) {
-    res.type("json");
-    res.send("[1,2,3]");
-    // Content-Type: application/json
+app.use("/buckeroo",function(req,res) {
+    res.setHeader("Etag","buckeroo");
+    res.send("buckeroo");
 });
 app.listen(4000);

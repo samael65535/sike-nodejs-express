af1d670d185e03191f662b5354b0392d81e63a87
samael <samael.65535@gmail.com>
2015-07-22 00:40:13 +0800

    Last-Modified 304

--- bin/myexpress.js
+++ bin/myexpress.js
@@ -7,7 +7,7 @@ app.use("/plumless",function(req,res) {
     res.send("plumless");
 });
 app.use("/",function(req,res) {
-    res.setHeader("Etag","foo-v1")
-    res.send("foo-v1");
+    res.setHeader("Last-Modified","Sun, 31 Jan 2010 16:00:00 GMT")
+    res.send("bar-2010");
 });
 app.listen(4000);

345149db330ce93220ee2dfe39babef2b581b4ef
samael <samael.65535@gmail.com>
2015-07-26 14:03:10 +0800

    di

--- bin/myexpress.js
+++ bin/myexpress.js
@@ -4,8 +4,7 @@ var makeRoute = require('../lib/route');
 var inject = require("../lib/injector");
 var app = express();
 var fs = require('fs');
-app.use("/",function(req,res) {
-    file = fs.createReadStream("../verify/fixtures/data.txt");
-    res.stream(file);
+app.use(function(req,res) {
+    res.sendfile(path,options);
 });
 app.listen(4000);

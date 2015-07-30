ed77b87a958ea1df1c54d4c95b65e21f682fd094
samael <samael.65535@gmail.com>
2015-07-30 18:37:55 +0800

    注释调试行

--- bin/myexpress.js
+++ bin/myexpress.js
@@ -5,12 +5,12 @@ var inject = require("../lib/injector");
 var app = express();
 var fs = require('fs');
 var request = require("supertest");
-function get(path,options) {
-    app.use(function(req,res) {
-        res.sendfile(__dirname + "/../verify/fixtures/data.txt",options);
-    });
-    return request(app).get("/");
-}
-get(__dirname);
+//function get(path,options) {
+//    app.use(function(req,res) {
+//        res.sendfile(__dirname + "/../verify/fixtures/data.txt",options);
+//    });
+//    return request(app).get("/");
+//}
+//get(__dirname);
 
 app.listen(4000);
